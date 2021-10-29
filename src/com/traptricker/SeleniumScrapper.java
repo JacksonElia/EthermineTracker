d9522ce920a3d9e8f99c0f08f917b2c15adfe498
Traptricker <jackgeorgeelia@gmail.com>
2021-10-29 12:17:12 -0400

    Got Selenium to work

--- src/com/traptricker/SeleniumScrapper.java
+++ src/com/traptricker/SeleniumScrapper.java
@@ -1,13 +1,14 @@
 package com.traptricker;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
+import java.util.List;
 
 public class SeleniumScrapper {
 
-    public void getEthermineHTML(String minerAddress) throws InterruptedException {
+    public List<String> getEthermineHTML(String minerAddress) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
@@ -16,11 +17,16 @@ public class SeleniumScrapper {
         driver.get(ethermineURL);
         // Waits to make sure the web page has loaded
         Thread.sleep(10000);
-        String search = driver.findElement(By.xpath("//div[@class='active table-container']//tbody")).getText();
-
-        System.out.println(search);
 
         // Turns on auto refreshing for the page
         driver.findElement(By.xpath("//div[@class='slider round']")).click();
+        List<WebElement> searchList = driver.findElements(By.xpath("//div[@class='active table-container']//tbody"));
+
+        // Confirms that there are active miners, then gets each of them
+        if (!searchList.isEmpty()) {
+            String search = searchList.get(0).getText();
+            return List.of(search.split("\n"));
+        }
+        return null;
     }
 }
